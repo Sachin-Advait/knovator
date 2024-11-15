1a589972fdaa0330c2401dc3872f0a5a0052e812
Sachin Kumar <sachinadvaita@gmail.com>
2024-11-15 17:01:57 +0530

    Updated Getit.

--- lib/get_it.dart
+++ lib/get_it.dart
@@ -1,4 +1,7 @@
 import 'package:get_it/get_it.dart';
+import 'package:knovator/database/app_database.dart';
+import 'package:knovator/modules/post_details/bloc/post_details_bloc.dart';
+import 'package:knovator/modules/post_details/repository/post_details_repository.dart';
 import 'package:knovator/modules/posts/bloc/posts_bloc.dart';
 import 'package:knovator/modules/posts/repository/posts_repository.dart';
 import 'package:knovator/modules/splash/bloc/splash_bloc.dart';
@@ -12,18 +15,33 @@ void getItSetup() {
 
   getIt.registerLazySingleton<ApiClient>(() => DioApiClient());
 
+  /// ------------- Register a lazy singleton for Database. -------------
+
+  getIt.registerLazySingleton<AppDatabase>(
+    () => AppDatabase(),
+  );
+
   /// ------------- Register a lazy singleton for Repositories. -------------
-  ///
+
   getIt.registerLazySingleton<PostsRepository>(
     () => PostsRepository(getIt()),
   );
 
+  getIt.registerLazySingleton<PostDetailsRepository>(
+    () => PostDetailsRepository(getIt()),
+  );
+
   /// ------------- Register a factory for BLOCs. -------------
+
   getIt.registerFactory<SplashBloc>(
     () => SplashBloc(),
   );
 
   getIt.registerFactory<PostsBloc>(
-    () => PostsBloc(getIt()),
+    () => PostsBloc(getIt(), getIt()),
+  );
+
+  getIt.registerFactory<PostDetailsBloc>(
+    () => PostDetailsBloc(getIt(), getIt()),
   );
 }
