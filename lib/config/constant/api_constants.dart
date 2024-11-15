93e288b78d6927b3000b9340866fdf79193d613d
Sachin Kumar <sachinadvaita@gmail.com>
2024-11-15 17:01:56 +0530

    Api endpoints, string and error files.

---
+++ lib/config/constant/api_constants.dart
@@ -0,0 +1,12 @@
+// ignore_for_file: constant_identifier_names
+
+class ApiConstants {
+  // API BASE URL..
+  static const String BASE_URL = 'https://jsonplaceholder.typicode.com/';
+
+  // API ENDPOINTS..
+
+  // Auth APIs
+  static const String POSTS = 'posts';
+  static const String SINGLE_POST = 'posts/';
+}
